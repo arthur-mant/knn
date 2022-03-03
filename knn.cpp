7b22cab9bd0f018a68a12b1cd291bdd8db202a52
Arthur Martinelli <ama18@inf.ufpr.br>
2022-03-03 00:15:28 -0300

    acho q ta funcionando

--- knn.cpp
+++ knn.cpp
@@ -20,10 +20,12 @@ int main(int argc, char **argv) {
     std::vector<std::array<double, 132>> train, test;
     std::vector<int> classes;
     std::array<double, 132> arr;
-    int classe, k, n, tam_treinamento, sum, max_class_neighbor, predicted_class;
-    std::vector<int> min_dist, min_dist_pos, class_vector;
+    int classe, k, n, tam_treinamento, max_class_neighbor, predicted_class;
+    std::vector<int> min_dist_pos, class_vector;
     std::vector<std::vector<int>> confusao;
     bool done;
+    double sum, accuracy;
+    std::vector<double> min_dist;
 
     //lendo treinamento
     tam_treinamento = 0;
@@ -64,7 +66,7 @@ int main(int argc, char **argv) {
     }
     min_dist.resize(k);
     min_dist_pos.resize(k);
-    class_vector.resize(k);
+    class_vector.resize(n);
     //lendo teste e calculando classe
     myfile.open(argv[2]);
 
@@ -87,22 +89,38 @@ int main(int argc, char **argv) {
 
             }
             //arr contem uma entrada da base de teste
-            sum = 0;
             fill(min_dist_pos.begin(), min_dist_pos.end(), 0);
             max_class_neighbor = 0;
             for (int i=0; i<tam_treinamento; i++) {
-                for (int j=0; j<132; j++)
+                sum = 0;
+                for (int j=0; j<132; j++) {
                     sum += (train.at(i)[j] - arr[j])*(train.at(i)[j] - arr[j]);
+                }
+//                std::cout << i << "\n";
+/*
+                std::cout << sum << " vs ";
+                for (auto j: min_dist)
+                    std::cout << j << ", ";
+                std::cout << "\n";
+*/
                 if (i<k) {
+//                    std::cout << "i<k sum = " << sum << "\n";
+                    
                     min_dist.at(i) = sum;
                     min_dist_pos.at(i) = i;
                 }
                 else {
+/*
+                    for(auto var: min_dist)
+                        std::cout << var << ", ";
+                    std::cout << "\n";
+*/
                     int j=0;
                     done = false;
                     while ((j<k) && !(done)) {
                     
                         if (sum < min_dist.at(j)) {
+//                            std::cout << "on if " << j << "\n";
                             min_dist.at(j) = sum;
                             min_dist_pos.at(j) = i;
                             done = true;
@@ -112,17 +130,24 @@ int main(int argc, char **argv) {
                 }
             }
             fill(class_vector.begin(), class_vector.end(), 0);
+/*
+            for (auto i: min_dist_pos)
+                std::cout << i << ", ";
+            std::cout << "\n";
+*/
             for (int i=0; i<k; i++)
                class_vector.at(classes.at(min_dist_pos.at(i)))++;
+//            std::cout << "a\n";
             for (int i=0; i<k; i++) {
                 if (class_vector.at(i) > max_class_neighbor) {
                     max_class_neighbor = class_vector.at(i);
                     predicted_class = i;
                 }
             }
+//            std::cout << "b\n";
             //confusao[real][predita]
             confusao.at(classe).at(predicted_class)++;
-            std::cout << "classe: " << classe << " classe prevista: " << predicted_class << "\n";
+//            std::cout << "classe: " << classe << " classe prevista: " << predicted_class << "\n";
         }
     }
     myfile.close();
@@ -132,5 +157,8 @@ int main(int argc, char **argv) {
             std::cout << confusao.at(i).at(j) << ", ";
         std::cout << "]\n";
     }
-
+    accuracy = 0;
+    for (int i=0; i<n; i++)
+        accuracy += confusao.at(i).at(i);
+    std::cout << "accuracy: " << 100*accuracy/test.size() << "\n";
 }
